14168b87dcff6002145563ec745281bbc4ce2c97
Sebastian Ojefors <godnoken@gmail.com>
2023-08-24 19:31:06 +0200

    Add reveal image with circle

---
+++ src/javascript/canvas.js
@@ -0,0 +1,52 @@
+const avatarCanvas = document.querySelector(".avatar-canvas");
+const opacityCanvas = document.querySelector(".opacity-canvas");
+const canvasContainer = document.querySelector(".canvas-container");
+const avatarContext = avatarCanvas.getContext("2d");
+const opacityContext = opacityCanvas.getContext("2d");
+const localImage = document.querySelector(".local-image-input");
+const img = new Image();
+
+// Make sure canvases actual size is the same as the CSS canvas (avoids oval circle)
+avatarCanvas.width = canvasContainer.offsetWidth;
+avatarCanvas.height = canvasContainer.offsetHeight;
+opacityCanvas.width = canvasContainer.offsetWidth;
+opacityCanvas.height = canvasContainer.offsetHeight;
+
+let circleRadius = 100;
+let lastX = opacityCanvas.width / 2;
+let lastY = opacityCanvas.height / 2;
+
+opacityCanvas.addEventListener("mousemove", (event) => {
+  revealImage(event);
+});
+
+function revealImage(event) {
+  const rect = opacityCanvas.getBoundingClientRect();
+  const x =
+    ((event.clientX - rect.left) / (rect.right - rect.left)) *
+    opacityCanvas.width;
+  const y =
+    ((event.clientY - rect.top) / (rect.bottom - rect.top)) *
+    opacityCanvas.height;
+
+  // Make sure we only update the y or x pos as long as we are inside the boundaries of the image
+  if (y + circleRadius < opacityCanvas.height && y - circleRadius > 0) {
+    lastY = y;
+  }
+  if (x + circleRadius < opacityCanvas.width && x - circleRadius > 0) {
+    lastX = x;
+  }
+
+  opacityContext.clearRect(0, 0, opacityCanvas.width, opacityCanvas.height);
+
+  // Draws a dark transparent rect over image
+  opacityContext.globalCompositeOperation = "source-over";
+  opacityContext.fillStyle = "rgba(0, 0, 0, 0.9)";
+  opacityContext.fillRect(0, 0, opacityCanvas.width, opacityCanvas.height);
+
+  // Reveals image in a circle around the mouse when hovering the image
+  opacityContext.globalCompositeOperation = "destination-out";
+  opacityContext.beginPath();
+  opacityContext.arc(lastX, lastY, circleRadius, 0, 2 * Math.PI);
+  opacityContext.fill();
+}
