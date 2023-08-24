05081960178085d5d660914f48386683021e07e9
Sebastian Ojefors <godnoken@gmail.com>
2023-08-24 22:32:10 +0200

    Add image requirement checks

--- src/javascript/image.js
+++ src/javascript/image.js
@@ -1,14 +1,19 @@
+let URL = "";
+let image = new Image();
+
 export function readLocalFile(event) {
   return new Promise((resolve, reject) => {
     const reader = new FileReader();
-    reader.onload = () => resolve(reader.result);
+    reader.onload = () => {
+      URL = reader.result;
+      resolve(reader.result);
+    };
     reader.onerror = () => reject("Failed to upload image");
     reader.readAsDataURL(event.target.files[0]);
   });
 }
 
 export function loadImage(imageData) {
-  let image = new Image();
   image.src = imageData;
 
   return new Promise((resolve, reject) => {
@@ -18,9 +23,9 @@ export function loadImage(imageData) {
 }
 
 export function isPng() {
-  return true;
+  return URL.startsWith("data:image/png");
 }
 
 export function is512x512() {
-  return true;
+  return image.width === 512 && image.height === 512 ? true : false;
 }
