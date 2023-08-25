62fedaf0df74eda04f79c5e31bb45ebe371ebaac
Sebastian Ojefors <godnoken@gmail.com>
2023-08-25 15:16:51 +0200

    Remove axios & resize comment

--- src/javascript/status.js
+++ src/javascript/status.js
@@ -1,6 +1,6 @@
 export const initText = "Please upload a local or online image below.";
 export const controlsText =
-  "Left click on the image to confirm, scroll to expand and shrink.";
+  "Left click on the image to confirm your desired avatar.";
 export const wrongFormatText =
   "Your image has to be a .png and 512x512 in size. Click convert below to proceed.";
 export const successText = "Success! Enjoy your new avatar.";
